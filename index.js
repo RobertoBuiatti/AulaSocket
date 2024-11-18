5c08b304faa41a59214190ca01b29ebdf9fbd19f
Roberto <robertobuiatti2@gmail.com>
2024-11-18 14:15:57 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const { Server} = require("socket.io")
+
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
 
 app.get('/', (req, res)=>{
-    return `<h1>Hello Word</h1>`
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('a new client connected')
 })
 
 server.listen(3000, ()=>{
